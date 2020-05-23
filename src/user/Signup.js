ee161f19fc55f5a346deb1e422c7a20467f064e0
Tanuj upadhyay <upadhyaytanuj123@gmail.com>
2020-05-23 08:17:43 +0530

    we add signin feacter which work fine bi=ut we need some more stuff here to get more usefull

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -89,7 +89,7 @@ const Signup = () => {
             className="alert alert-success"
             style={{ display: success ? "" : "none" }}
           >
-            New account was created successfully. Please{" "}
+            New account was created successfully. Please
             <Link to="/signin">Login Here</Link>
           </div>
         </div>
